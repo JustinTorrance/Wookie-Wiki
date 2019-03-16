20f4ff5d6e349905d0cd3661141d82f163eb4a9d
Justin Torrance <justin.torrance91@gmail.com>
2019-03-15 18:26:18 -0600

    Randomize episode on page load.

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,10 +1,11 @@
-const randomizeEpisode = () => {
-    return Math.random()
+var randomizeEpisode = () => {
+    return Math.ceil(Math.random() * 7)
 }
 
 export const textFetch = async () => {
+    const episode = randomizeEpisode()
     try {
-        const response = await fetch('https://swapi.co/api/films/1/')
+        const response = await fetch(`https://swapi.co/api/films/${episode}/`)
         const film = await response.json()
         console.log(film)
         return film;
