6e3c722e73fa88896bd40d4dab7d8f9730d14d89
Justin Torrance <justin.torrance91@gmail.com>
2019-03-17 14:14:49 -0600

    Make API call for people

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -2,7 +2,7 @@ var randomizeEpisode = () => {
     return Math.ceil(Math.random() * 7)
 }
 
-export const textFetch = async () => {
+export const fetchText = async () => {
     const episode = randomizeEpisode()
     try {
         const response = await fetch(`https://swapi.co/api/films/${episode}/`)
@@ -11,4 +11,25 @@ export const textFetch = async () => {
     } catch (error) {
         console.log(error)
     } 
+}
+
+export const fetchPeople = async () => {
+    try {
+        const response = await fetch('https://swapi.co/api/people')
+        const peopleData = await response.json()
+        const people = await fetchHomeworlds(peopleData.results)
+        return people
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const fetchHomeworlds = (people) => {
+    const peopleData = people.map( async person => {
+        const response = await fetch(person.homeworld)
+        const homeworld = await response.json()
+        const homeworldData = {homeworld: homeworld.name, population: homeworld.population, person: person.name}
+        return homeworldData
+    })
+    return Promise.all(peopleData)
 }
